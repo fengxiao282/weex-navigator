b458861b04a171104e6aaa228a3e7c11b6f0bb80
heuulzp <heuulzp@163.com>
2017-10-10 10:13:15 +0800

    navigator方式跳转

---
+++ src/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper" @click="update">
+    <image :src="logoUrl" class="logo"></image>
+    <text class="title">click me to new page</text>
+  </div>
+</template>
+
+<style>
+  .wrapper { align-items: center; margin-top: 120px; }
+  .title { padding-top:40px; padding-bottom: 40px; font-size: 48px; }
+  .logo { width: 360px; height: 156px; }
+  .desc { padding-top: 20px; color:#888; font-size: 24px;}
+</style>
+
+<script>
+  var navigator = weex.requireModule('navigator')
+  var modal = weex.requireModule('modal')
+
+  export default {
+    mounted: function () {
+      console.log('url:', weex.config.bundleUrl)
+    },
+    data () {
+      return {
+        logoUrl: 'http://img1.vued.vanthink.cn/vued08aa73a9ab65dcbd360ec54659ada97c.png',
+        target: 'World'
+      }
+    },
+    methods: {
+      update: function (event) {
+        navigator.push({
+          url: 'http://192.168.3.85:8088/weex/new.js',
+          animated: "true"
+        }, event => {
+          modal.toast({ message: 'callback: ' + event })
+        })
+      }
+    }
+  }
+</script>
